ead7f8daa6f471c5363bb1489c5a3d015d441cb7
Maxwell Lyu <xlworkshop.chs@hotmail.com>
2020-05-20 21:28:28 +0800

    initial commit

---
+++ OnePlayer.cpp
@@ -0,0 +1,15 @@
+﻿#include "OnePlayer.h"
+
+OnePlayer::OnePlayer(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+    ui.tabWidget->tabBar()->hide();
+    connect(ui.pushButtonList, &QPushButton::clicked, this, [=] {onSwitchTab(0);});
+    connect(ui.pushButtonPlayer, &QPushButton::clicked, this, [=]{onSwitchTab(1);});
+    connect(ui.pushButtonLyric, &QPushButton::clicked, this, [=]{onSwitchTab(2);});
+}
+
+void OnePlayer::onSwitchTab(int index) {
+    ui.tabWidget->setCurrentIndex(index);
+}
---
+++ OnePlayer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include <QTabBar>
+#include <QFile>
+#include "ui_OnePlayer.h"
+
+class OnePlayer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    OnePlayer(QWidget *parent = Q_NULLPTR);
+
+private:
+    Ui::OnePlayerClass ui;
+private slots:
+    void onSwitchTab(int index);
+};
