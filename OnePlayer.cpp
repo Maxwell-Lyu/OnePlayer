f04c9b77ec52c007e392e5f91fb687b9944f2850
Maxwell Lyu <xlworkshop.chs@hotmail.com>
2020-05-22 22:45:11 +0800

    add: player basic

--- OnePlayer.cpp
+++ OnePlayer.cpp
@@ -3,18 +3,187 @@
 OnePlayer::OnePlayer(QWidget* parent)
 	: QMainWindow(parent),
 	mediaPlayer(nullptr),
-	mediaPlaylist(nullptr),
-	tabWidget(nullptr),
-	buttonGroupTab(nullptr)
+	mediaPlaylist(nullptr)
 {
 	ui.setupUi(this);
 	mediaPlayer = new QMediaPlayer(this);
 	mediaPlaylist = new QMediaPlaylist(this);
-	tabWidget = ui.tabWidget;
-	buttonGroupTab = ui.buttonGroupTab;
-	tabWidget->tabBar()->hide();
+	mediaPlaylist->setPlaybackMode(QMediaPlaylist::Loop); //循环模式
+	mediaPlayer->setPlaylist(mediaPlaylist);
+	connect(mediaPlayer, SIGNAL(stateChanged(QMediaPlayer::State)),
+		this, SLOT(onStateChanged(QMediaPlayer::State)));
+
+	connect(mediaPlayer, SIGNAL(positionChanged(qint64)),
+		this, SLOT(onPositionChanged(qint64)));
+
+	connect(mediaPlayer, SIGNAL(durationChanged(qint64)),
+		this, SLOT(onDurationChanged(qint64)));
+
+	connect(mediaPlaylist, SIGNAL(currentIndexChanged(int)),
+		this, SLOT(onPlaylistChanged(int)));
+
+	ui.tabWidget->tabBar()->hide();
 }
 
-void OnePlayer::on_btnList_clicked() { tabWidget->setCurrentIndex(0); }
-void OnePlayer::on_btnPlayer_clicked() { tabWidget->setCurrentIndex(1); }
-void OnePlayer::on_btnLyric_clicked() { tabWidget->setCurrentIndex(2); }
+
+
+
+
+void OnePlayer::onStateChanged(QMediaPlayer::State state)
+{//播放器状态变化，更新按钮状态
+	//ui.btnPlay->setEnabled(!(state == QMediaPlayer::PlayingState));
+	//ui.btnPause->setEnabled(state == QMediaPlayer::PlayingState);
+	ui.btnStop->setEnabled(state == QMediaPlayer::PlayingState);
+}
+
+void OnePlayer::onPlaylistChanged(int position)
+{//播放列表变化,更新当前播放文件名显示
+	ui.listMusic->setCurrentRow(position);
+	QListWidgetItem* item = ui.listMusic->currentItem();
+	//if (item)
+	//	ui.LabCurMedia->setText(item->text());
+}
+
+void OnePlayer::onDurationChanged(qint64 duration)
+{//文件时长变化，更新进度显示
+	ui.sliderProgress->setMaximum(duration);
+
+	//int   secs = duration / 1000;//秒
+	//int   mins = secs / 60; //分钟
+	//secs = secs % 60;//余数秒
+	//durationTime = QString::asprintf("%d:%d", mins, secs);
+	//ui.LabRatio->setText(positionTime + "/" + durationTime);
+}
+
+void OnePlayer::onCurrentMediaChanged(const QMediaContent& media) {
+	// TODO
+}
+
+
+
+void OnePlayer::onPositionChanged(qint64 position)
+{//当前文件播放位置变化，更新进度显示
+	if (ui.sliderProgress->isSliderDown())
+		return;
+
+	ui.sliderProgress->setSliderPosition(position);//
+
+	//int   secs = position / 1000;//秒
+	//int   mins = secs / 60; //分钟
+	//secs = secs % 60;//余数秒
+	//positionTime = QString::asprintf("%d:%d", mins, secs);
+	//ui.LabRatio->setText(positionTime + "/" + durationTime);
+}
+
+void OnePlayer::on_btnAdd_clicked()
+{//添加文件
+	QString curPath = QDir::homePath();//获取系统当前目录
+	QString dlgTitle = "选择音频文件"; //对话框标题
+	QString filter = "音频文件(*.mp3 *.wav *.wma);;mp3文件(*.mp3);;wav文件(*.wav);;wma文件(*.wma);;所有文件(*.*)"; //文件过滤器
+	QStringList fileList = QFileDialog::getOpenFileNames(this, dlgTitle, curPath, filter);
+
+	if (fileList.count() < 1)
+		return;
+
+	for (int i = 0; i < fileList.count(); i++)
+	{
+		QString aFile = fileList.at(i);
+		mediaPlaylist->addMedia(QUrl::fromLocalFile(aFile));//添加文件
+
+		QFileInfo   fileInfo(aFile);
+		ui.listMusic->addItem(fileInfo.fileName());//添加到界面文件列表
+	}
+
+	if (mediaPlayer->state() != QMediaPlayer::PlayingState)
+		mediaPlaylist->setCurrentIndex(0);
+	//mediaPlayer->play();
+}
+
+void OnePlayer::on_btnPlay_clicked()
+{//播放
+	if (mediaPlayer->state() == QMediaPlayer::PlayingState)
+		mediaPlayer->pause();
+	else {
+		if (mediaPlaylist->currentIndex() < 0)
+			mediaPlaylist->setCurrentIndex(0);
+		mediaPlayer->play();
+	}
+}
+
+
+void OnePlayer::on_btnStop_clicked()
+{//停止播放
+	mediaPlayer->stop();
+}
+
+void OnePlayer::on_listMusic_doubleClicked(const QModelIndex& index)
+{//双击时切换播放文件
+	int rowNo = index.row();
+	mediaPlaylist->setCurrentIndex(rowNo);
+	mediaPlayer->play();
+}
+
+void OnePlayer::on_btnClear_clicked()
+{//清空列表
+	mediaPlaylist->clear();
+	ui.listMusic->clear();
+	mediaPlayer->stop();
+}
+
+void OnePlayer::on_sliderVolume_valueChanged(int value)
+{//调整音量
+	mediaPlayer->setVolume(value);
+}
+
+
+void OnePlayer::on_sliderProgress_valueChanged(int value)
+{//文件进度调控
+	mediaPlayer->setPosition(value);
+}
+
+void OnePlayer::on_btnRemove_clicked()
+{//移除一个文件
+	int pos = ui.listMusic->currentRow();
+	QListWidgetItem* item = ui.listMusic->takeItem(pos);
+	delete item;//从listMusic里删除
+
+	if (mediaPlaylist->currentIndex() == pos) //是当前播放的曲目
+	{
+		int nextPos = 0;
+		if (pos >= 1)
+			nextPos = pos - 1;
+
+		mediaPlaylist->removeMedia(pos);//从播放列表里移除
+		if (ui.listMusic->count() > 0)
+		{
+			mediaPlaylist->setCurrentIndex(nextPos);
+			onPlaylistChanged(nextPos);
+		}
+		else
+		{
+			mediaPlayer->stop();
+			//ui.LabCurMedia->setText("无曲目");
+		}
+	}
+	else
+		mediaPlaylist->removeMedia(pos);
+}
+
+void OnePlayer::on_btnPrev_clicked()
+{//前一文件
+	mediaPlaylist->previous();
+}
+
+void OnePlayer::on_btnNext_clicked()
+{//下一文件
+	mediaPlaylist->next();
+}
+
+
+
+
+
+
+void OnePlayer::on_btnList_clicked() { ui.tabWidget->setCurrentIndex(0); }
+void OnePlayer::on_btnPlayer_clicked() { ui.tabWidget->setCurrentIndex(1); }
+void OnePlayer::on_btnLyric_clicked() { ui.tabWidget->setCurrentIndex(2); }
--- OnePlayer.h
+++ OnePlayer.h
@@ -6,6 +6,8 @@
 #include <QMediaPlayer>
 #include <QMediaPlaylist>
 #include <QDebug>
+#include <QFileDialog>
+#include <deque>
 #include "ui_OnePlayer.h"
 
 class OnePlayer : public QMainWindow
@@ -19,11 +21,26 @@ private slots:
 	void on_btnPlayer_clicked();
 	void on_btnLyric_clicked();
 
+	void onStateChanged(QMediaPlayer::State state);
+	void onPlaylistChanged(int position);
+	void onDurationChanged(qint64 duration);
+	void onPositionChanged(qint64 position);
+	void onCurrentMediaChanged(const QMediaContent& media);
+
+	//
+	void on_btnAdd_clicked();
+	void on_btnPlay_clicked();
+	void on_btnStop_clicked();
+	void on_listMusic_doubleClicked(const QModelIndex& index);
+	void on_btnClear_clicked();
+	void on_sliderVolume_valueChanged(int value);
+	void on_sliderProgress_valueChanged(int value);
+	void on_btnRemove_clicked();
+	void on_btnPrev_clicked();
+	void on_btnNext_clicked();
+
 private:
 	Ui::OnePlayerClass ui;
 	QMediaPlayer* mediaPlayer;      // 播放器
 	QMediaPlaylist* mediaPlaylist;  // 播放列表
-	QTabWidget* tabWidget;          // 选项卡区域
-	QButtonGroup* buttonGroupTab;   // 选项卡切换按钮
-
 };
